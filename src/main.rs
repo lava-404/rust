95a80f0e477e10910c4af5c396016021ac43fc54
lavanya <lavanyaropt@gmail.com>
2026-05-28 02:36:22 +0530

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
-struct Node {
-    data: i32,
-    next: i32
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
 
-struct LinkedList {
-    linked_list: Vec<Node>
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
 }
 
-
-impl LinkedList {
-    fn new() -> Self {
-       Self { linked_list: vec![] }
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
     }
 
-    fn add(&mut self, val: i32, addr: i32){
-       if let Some(node) = self.linked_list.last_mut(){
-        node.next = addr;
-        let n  = Node{
-            data: val,
-            next: 0
-        };
-        self.linked_list.push(n);
-       }
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
     }
 }
